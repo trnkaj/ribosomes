4c4fa483a79d15f545ba646a5926a2e18be94321
Jan Trnka <98970528+trnkaj@users.noreply.github.com>
2026-01-18 11:24:16 +0100

    Amended version of the script uploaded
    Added the visualisation and explicit print out of the slope parameter in the linear model.

--- figure1c/TNTs-gemcitabine.R
+++ figure1c/TNTs-gemcitabine.R
@@ -2,7 +2,8 @@ library(rethinking)
 library(tidyverse)
 
 library(readxl)
-TNTs_gem <- read_excel("./figure1c/TNTs-gem.xlsx", 
+
+TNTs_gem <- read_excel("~/Downloads/ribosomes-main/figure1c/TNTs-gemcitabine.xlsx", 
                        skip = 1)
 
 #filter data
@@ -12,7 +13,7 @@ stddata <- list(
   gemstd = standardize(data$gem),
   totstd = standardize(data$tot)
 )
-stddata
+#stddata
 
 #estimator
 bTNT <- ulam(
@@ -22,7 +23,7 @@ bTNT <- ulam(
     a ~ dnorm(0,1),
     b ~ dnorm(0,1),
     sd ~ dexp(1)
-  ), data=stddata, chains = 4, cores = 4
+  ), data=stddata, chains = 4, cores = 4, iter=4000
   
 )
 
@@ -30,23 +31,30 @@ precis(bTNT)
 
 #posterior distributions
 post <- extract.samples(bTNT)
-post
+#post
 #graph standardised coefficients
 dens(post$a)
 dens(post$b)
 #unstandardise coefficient b
+tot_sd <- sd(TNTs_gem$`total count`)
+tot_mean <- mean(TNTs_gem$`total count`)
 b_ustd <- post$b*tot_sd/gem_sd
+b_ustd_mean <-  mean(b_ustd)
+b_ustd_cri95 <- HPDI(b_ustd, prob=0.95)
+
+print(paste("Estimated increase in TNTs per μM Gemcitabine:", round(b_ustd_mean, 2), 
+            " (95% CrI:", round(b_ustd_cri95[1], 2), ",", round(b_ustd_cri95[2], 2), ")"))
+
 #graph unstandardised coefficient b
-dens(b_ustd, show.zero = TRUE, #main="Posterior distibution of slope"
-     ylab="", xlab="", cex.axis=1.5)
+dens(b_ustd, show.zero = TRUE, main="Posterior distribution of regression slope",
+     ylab="", xlab="Change in TNTs per μM Gemcitabine", cex.axis=1, adj=1.5, xlim=c(-20,300))
+shade(density(b_ustd, adj=1.5), HPDI(b_ustd, prob=0.89))
 
 #check priors
 prior <- extract.prior(bTNT)
 mu <- link(bTNT, post=prior, data=list(gemstd=c(-2,2)))
 plot(NULL, xlim=c(-2,2),ylim=c(-2,2))
 for (i in 1:50) lines (c(-2,2), mu[i,], col=col.alpha("black",0.4))
-
-#plot(stddata$totstd~stddata$gemstd)
 
 #posterior prediction
 plot(data$tot~data$gem,xlab="", ylab="", cex.lab = 1.5, 
@@ -60,16 +68,16 @@ gem <- seq(from=-1.15, to = 1.5, length.out=ns)
 gem_mean <- mean(TNTs_gem$`gemcitabine (uM)`)
 gem_sd <- sd(TNTs_gem$`gemcitabine (uM)`)
 gem_destand <- gem*gem_sd+gem_mean
-gem_destand
+#gem_destand
 #link model
 mu <- link(bTNT, data=list(gemstd=gem))
 tot_stand_mu <- apply(mu, 2, mean)
 tot_stand_pi <- apply(mu, 2, PI)
 #destandardise output values
-tot_sd <- sd(TNTs_gem$`total count`)
-tot_mean <- mean(TNTs_gem$`total count`)
 tot_mu <- tot_stand_mu*tot_sd+tot_mean
 tot_pi <- tot_stand_pi*tot_sd+tot_mean
 #plot prediction lines
 lines(gem_destand, tot_mu, lty=2, lwd=1.5)
 shade(tot_pi, gem_destand, xpd=TRUE)
+mtext(paste("Estimated increase in TNTs per μM Gemcitabine:", round(b_ustd_mean, 2), 
+      " (95% CrI:", round(b_ustd_cri95[1], 2), ",", round(b_ustd_cri95[2], 2), ")"), side=3, line=0.5, cex=1.2)
